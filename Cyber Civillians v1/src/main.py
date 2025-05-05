bae7c54d5cb346b6c45be544b3058d23253ce66b
omg29 <139054266+omg29@users.noreply.github.com>
2025-05-05 16:51:49 +0000

    player and main game setup

--- Cyber Civillians v1/src/main.py
+++ Cyber Civillians v1/src/main.py
@@ -1,1 +1,42 @@
-print ("Hello World")
+#import all libraries
+import pygame
+from pygame.locals import *
+from pygame import mixer
+
+#import all files
+
+#make the game
+pygame.init()
+pygame.mixer.pre_init(buffer = 1024)
+game_width = 1000
+game_heigh = 600
+screen = pygame.display.set_mode((game_width, game_heigh))
+clock = pygame.time.Clock()
+running = True
+game_started = False
+
+#start the game
+def startGame():
+    pass
+
+#==================================================================#
+#============================LOOP LAND=============================#
+#==================================================================#
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+            running = False
+    #screen.blit(background_image, (0, 0))
+    if not game_started:
+        events = pygame.event.get()
+        for event in events:
+            if event.type == pygame.KEYDOWN:
+                startGame()
+                break
+    
+    #game functions
+    pygame.display.flip()
+    clock.tick(40)
+    pygame.display.set_caption("Cyber Civillians fps: " + str(clock.get_fps))
