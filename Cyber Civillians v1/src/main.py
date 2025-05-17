c5b9ef8f9e1045984030efec8b6317e87699a364
omg29 <139054266+omg29@users.noreply.github.com>
2025-05-17 18:45:07 -0400

    changes comign soon

---
+++ Cyber Civillians v1/src/main.py
@@ -0,0 +1,45 @@
+#import all libraries
+import pygame
+from pygame.locals import *
+from pygame import mixer
+
+#import all files
+
+#make the game
+pygame.init()
+pygame.mixer.pre_init(buffer = 1024)
+game_width = 1200
+game_heigh = 675
+screen = pygame.display.set_mode((game_width, game_heigh))
+clock = pygame.time.Clock()
+running = True
+game_started = False
+
+#start the game
+def startGame():
+    pass
+
+#load background
+background_image = pygame.image.load("./assets/images/BG_SciFi.png")
+
+#==================================================================#
+#============================LOOP LAND=============================#
+#==================================================================#
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+            running = False
+    screen.blit(background_image, (0, 0))
+    if not game_started:
+        events = pygame.event.get()
+        for event in events:
+            if event.type == pygame.KEYDOWN:
+                startGame()
+                break
+    
+    #game functions
+    pygame.display.flip()
+    clock.tick(40)
+    pygame.display.set_caption("Cyber Civillians fps: " + str(clock.get_fps))
